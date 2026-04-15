459e572205d333de5e0d221545c5022dd6183f2f
kalpanabaivadithya <kalpanabaivadithya@gmail.com>
2026-04-15 20:51:37 +0530

    number

---
+++ number.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+# -------------------------------
+# WEATHER DATA (7 Days)
+# -------------------------------
+# Temperatures in Celsius
+temps = np.array([32, 35, 30, 31, 36, 34, 33])
+
+# -------------------------------
+# FUNCTIONS
+# -------------------------------
+
+def average_temp(data):
+    return np.mean(data)
+
+def max_temp(data):
+    return np.max(data)
+
+def min_temp(data):
+    return np.min(data)
+
+def hot_days(data):
+    return np.where(data > 33, "Hot", "Normal")
+
+def temperature_difference(data):
+    return np.max(data) - np.min(data)
+
+# -------------------------------
+# MAIN PROGRAM
+# -------------------------------
+
+print(" Weekly Temperature Data:", temps)
+
+avg = average_temp(temps)
+print("\n Average Temperature:", avg)
+
+print("\n Highest Temperature:", max_temp(temps))
+print(" Lowest Temperature:", min_temp(temps))
+
+status = hot_days(temps)
+print("\n Day Status (Hot/Normal):", status)
+
+diff = temperature_difference(temps)
+print("\n Temperature Difference:", diff)
